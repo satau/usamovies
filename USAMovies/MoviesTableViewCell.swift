a98ded91bdfe628396ad5d3cd87fda3f03b42852
Sherrie Ann Auer <sherrie.toboso@gmail.com>
2016-05-16 21:38:09 +0200

    image logic from the array or from the background thread has coded

--- USAMovies/MoviesTableViewCell.swift
+++ USAMovies/MoviesTableViewCell.swift
@@ -24,6 +24,44 @@ class MoviesTableViewCell: UITableViewCell {
     func updateCell(){
         titleLabel.text = movies?.vName
         rankLabel.text = ("\(movies!.vRank)")
-        movieImage.image = UIImage(named: "imageNotAvailable")
+        //movieImage.image = UIImage(named: "imageNotAvailable")
+        
+        if movies!.vImageData != nil{
+            print("get data from the array")
+            movieImage.image = UIImage(data: movies!.vImageData!)
+        }else{
+            print("print images in the background thread")
+            GetVideoImage(movies!, imageView: movieImage)
+        }
+        
+        
+    }
+    
+    
+    func GetVideoImage(movie: Movies, imageView : UIImageView){
+        
+        // Background thread
+        //  DISPATCH_QUEUE_PRIORITY_HIGH Items dispatched to the queue will run at high priority, i.e. the queue will be scheduled for execution before any default priority or low priority queue.
+        //
+        //  DISPATCH_QUEUE_PRIORITY_DEFAULT Items dispatched to the queue will run at the default priority, i.e. the queue will be scheduled for execution after all high priority queues have been scheduled, but before any low priority queues have been scheduled.
+        //
+        //  DISPATCH_QUEUE_PRIORITY_LOW Items dispatched to the queue will run at low priority, i.e. the queue will be scheduled for execution after all default priority and high priority queues have been scheduled.
+        
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            
+            let data = NSData(contentsOfURL: NSURL(string: self.movies!.vImageUrl)!)
+            
+            var image : UIImage?
+            if data != nil {
+                self.movies!.vImageData = data
+                image = UIImage(data: data!)
+            }
+            
+            // move back to Main Queue
+            dispatch_async(dispatch_get_main_queue()) {
+                imageView.image = image
+            }
+        }
     }
 }
